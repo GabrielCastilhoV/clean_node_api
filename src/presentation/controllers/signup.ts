5158a3ee38d520e2f359c766b42b2f0c504150bf
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-15 20:47:28 -0400

    refactor: add customize error and add interface from http

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,20 @@
+import { HttpResponse, HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
-    return {
-      statusCode: 400
+  handle (httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('name')
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('email')
+      }
     }
   }
 }
