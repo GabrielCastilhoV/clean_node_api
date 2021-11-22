5aa9c8216df59b60dde0ded71d5d0cfa79c82e15
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-22 18:40:24 -0400

    feat: create class DbAddAccount

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,16 @@
+import { IAccountModel } from '../../../domain/models/account'
+import { IAddAccount, IAddAccountModel } from '../../../domain/usecases/add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+export class DbAddAccount implements IAddAccount {
+  private readonly encrypter: Encrypter;
+
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (account: IAddAccountModel): Promise<IAccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return await new Promise(resolve => resolve(null))
+  }
+}
